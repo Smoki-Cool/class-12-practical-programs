58955d94ab229266faa74b0c702acb1cb5696e93
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-12-31 19:45:28 +0530

    Finalisation

--- program05.py
+++ program05.py
@@ -1,7 +1,6 @@
 # Write a Python program to demonstrate the concept of variable length argument
 # to calculate the product and power of the first 10 numbers.
 
-
 def cproduct(*args):
 	product = 1
 	for n in args:
@@ -10,7 +9,7 @@ def cproduct(*args):
 	return product
 
 def cpower(power=1, *args):
-	return [n ** power for n in args]
+	return [str(n ** power) for n in args]
 
 
 numbers = list(range(1, 11))
@@ -18,5 +17,5 @@ numbers = list(range(1, 11))
 rproduct = cproduct(*numbers)
 rpower = cpower(2, *numbers)
 
-print("Product of the first 10 numbers: ", rproduct)
-print("Power of the first 10 numbers(squared): ", rpower)
+print("Product of the first 10 numbers:", rproduct)
+print("Power of the first 10 numbers(squared):", ", ".join(rpower))
