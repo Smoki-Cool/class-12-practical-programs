61bf48573cf5ebd02508d09237a5e6c5b488f43b
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-12-31 20:15:07 +0530

    Finalisation

---
+++ program07.py
@@ -0,0 +1,22 @@
+# Write a python program to search any word in given string/sentence. User provides 
+# input string/sentence and the word to search.
+
+import re
+
+def search(sentence: str, word: str):
+    s = re.search(r"\b{0}".format(word), sentence)
+
+    if s:
+        mark(s.start(), word, sentence)
+    else:
+        print("No results found.")
+
+def mark(start, word, sentence):
+    print()
+    print(sentence)
+    print(" "*start + "^"*len(word))
+
+sentence = input("Enter your sentence: ")
+word = input("Enter the word to search for: ")
+
+search(sentence, word)
