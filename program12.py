356007f34e70b480713ec3e037c182b3daf53631
Smoki <64897895+Smoki-Cool@users.noreply.github.com>
2023-12-31 20:47:58 +0530

    Finalisation

--- program12.py
+++ program12.py
@@ -1,12 +1,11 @@
-# Write a python program to read the content of the file line by line and write it to the 
+# Write a python program to read the content of the file line by line and write it to
 # another file except for the lines that contain ‘o’ letter in it.
-
 
 with open("text.txt") as file1:
 	with open("copy.txt", "w") as file2:
 		count = 0
 		for line in file1.readlines():
-			if 'o' not in line:
+			if 'o' not in line.lower():
 				file2.write(line)
 				count += 1
 		
